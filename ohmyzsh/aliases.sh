54c2dca2f757d01e56beb25c7c7b2a581e7167a7
JonathanWu1 <wujo2012@gmail.com>
2025-03-30 12:44:23 +0200

    updated alias and path

--- ohmyzsh/aliases.sh
+++ ohmyzsh/aliases.sh
@@ -1,1 +1,11 @@
+alias zshconfig="nvim ~/.zshrc"
+alias ohmyzsh="nvim ~/.oh-my-zsh"
+alias aliasconfig="nvim ~/.oh-my-zsh/custom/aliases.zsh"
+alias ghosttyconfig="nvim ~/.config/ghostty/config"
+alias ghosttyupdate="cp ~/.config/ghostty/config ~/dotfiles/config"
+alias vpn="sudo openvpn --config ~/.config/vpn/OpenVPN/vpnconfig_cert.ovpn&"
+alias nvimconfig="cd ~/.config/nvim"
 
+alias hub="cd ~/cargonerds/cargonerds-hub/"
+alias jarvis="cd ~/cargonerds/jarvis/"
+alias azd="cd ~/cargonerds/docker/azurite/data/"
