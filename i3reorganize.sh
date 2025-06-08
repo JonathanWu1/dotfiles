3464756eeca3f7280ccab16f384be87167d338c7
JonathanWu1 <wujo2012@gmail.com>
2025-06-08 17:11:59 +0200

    i3 and ideavim

---
+++ i3reorganize.sh
@@ -0,0 +1,11 @@
+i3-msg '[class="Docker Desktop"] move container to workspace 9'
+i3-msg '[class="docker"] move container to workspace 9'
+i3-msg '[class="ghostty"] move container to workspace 1'
+i3-msg '[class="rider"] move container to workspace 2'
+i3-msg '[class="^Google-chrome$"] move container to workspace 3'
+i3-msg '[class="^Google-chrome$" title="^DevTools"] move container to workspace 6'
+i3-msg '[title="YouTube Music"] move container to workspace 4'
+i3-msg '[title="Microsoft Teams"] move container to workspace 4'
+i3-msg '[title="dotnet run"] move container to workspace 9'
+
+
