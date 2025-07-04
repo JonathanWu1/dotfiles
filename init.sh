9277df74e9d9c12647cd8e84bf9085add6a4f7dc
JonathanWu1 <wujo2012@gmail.com>
2025-07-04 22:15:41 +0200

    more stuff

--- init.sh
+++ init.sh
@@ -3,13 +3,13 @@
 sudo apt update
 sudo apt upgrade
 
-sudo apt install make unzip gcc ripgrep fzf xclip curl -y
+sudo apt install make unzip gcc ripgrep fzf xclip curl pass -y
 
 sudo snap install nvim --classic -y
 sudo snap install ghostty --classic -y
 
-
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 
 export PROFILE=~/.zshrc
